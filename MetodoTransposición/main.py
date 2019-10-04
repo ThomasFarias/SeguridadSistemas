b6bcc195f1f3d2adc7dc7924a4b2f5537fdcae2e
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2019-10-04 11:39:48 -0300

    eliminando clear_output

--- MetodoTransposición/main.py
+++ MetodoTransposición/main.py
@@ -5,13 +5,12 @@
 
 
 from Encriptador import Encriptador
-from IPython.display import clear_output
 e1=Encriptador()
 print("Ingrese texto a cifrar")
 texto = input()
 print("Ingrese clave")
 clave = input()
-clear_output()
+
 print('Texto cifrado:',e1.cifrar(texto,clave))
 
 
